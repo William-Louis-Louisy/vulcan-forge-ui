fbe4ea89fb978785857ebb6b04752d039d809701
William Louis-Louisy <87181045+William-Louis-Louisy@users.noreply.github.com>
2026-07-08 12:05:25 +0200

    feat(i18n): type merged component guideline messages

--- src/types/next-intl.d.ts
+++ src/types/next-intl.d.ts
@@ -1,9 +1,29 @@
 import type { routing } from '../i18n/routing';
-import type messages from '../messages/en.json';
+import type baseMessages from '../messages/en.json';
+import type { componentGuidelineMessages } from '../messages/component-guidelines';
+
+type DeepMerge<Left, Right> = {
+  [Key in keyof Left | keyof Right]: Key extends keyof Right
+    ? Key extends keyof Left
+      ? Left[Key] extends Record<string, unknown>
+        ? Right[Key] extends Record<string, unknown>
+          ? DeepMerge<Left[Key], Right[Key]>
+          : Right[Key]
+        : Right[Key]
+      : Right[Key]
+    : Key extends keyof Left
+      ? Left[Key]
+      : never;
+};
+
+type Messages = DeepMerge<
+  typeof baseMessages,
+  (typeof componentGuidelineMessages)['en']
+>;
 
 declare module 'next-intl' {
   interface AppConfig {
     Locale: (typeof routing.locales)[number];
-    Messages: typeof messages;
+    Messages: Messages;
   }
 }
