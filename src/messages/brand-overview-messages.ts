66ffa7caf415052997b2b754efda4de465bc9f3a
William Louis-Louisy <87181045+William-Louis-Louisy@users.noreply.github.com>
2026-07-24 14:20:29 +0200

    feat(ds-170-05): localize brand overview activity

---
+++ src/messages/brand-overview-messages.ts
@@ -0,0 +1,20 @@
+export const brandOverviewMessages = {
+  en: {
+    ProjectOverviewPage: {
+      activity: {
+        items: {
+          brand: 'Brand profile updated',
+        },
+      },
+    },
+  },
+  fr: {
+    ProjectOverviewPage: {
+      activity: {
+        items: {
+          brand: 'Profil de marque mis à jour',
+        },
+      },
+    },
+  },
+} as const;
