45808280b1db170eecf0a163b728eb77a9d1e5bd
William Louis-Louisy <87181045+William-Louis-Louisy@users.noreply.github.com>
2026-07-27 10:34:11 +0200

    refactor(ds-170-06): split public marketing and auth chrome

--- src/app/[locale]/(public)/layout.tsx
+++ src/app/[locale]/(public)/layout.tsx
@@ -1,15 +1,9 @@
 import type { ReactNode } from 'react';
-import { PublicHeader } from '@/components/layout/PublicHeader';
 
 type PublicLayoutProps = {
   children: ReactNode;
 };
 
 export default function PublicLayout({ children }: PublicLayoutProps) {
-  return (
-    <>
-      <PublicHeader />
-      {children}
-    </>
-  );
+  return children;
 }
