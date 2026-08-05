8d949a0fc7a34d0018e6db271b5015f891fee969
William Louis-Louisy <87181045+William-Louis-Louisy@users.noreply.github.com>
2026-08-05 14:52:18 +0200

    feat(auth): define email verification policy

---
+++ src/server/auth/email-verification/email-verification.constants.ts
@@ -0,0 +1,6 @@
+export const EMAIL_VERIFICATION_TOKEN_BYTES = 32;
+export const EMAIL_VERIFICATION_TOKEN_TTL_MS = 30 * 60_000;
+export const EMAIL_VERIFICATION_TOKEN_MAX_LENGTH = 128;
+
+export const EMAIL_VERIFICATION_DELIVERY_TIMEOUT_MS = 5_000;
+export const RESEND_EMAIL_ENDPOINT = 'https://api.resend.com/emails';
