d608317286cf11476472064b0955fa2e6b29763c
William Louis-Louisy <87181045+William-Louis-Louisy@users.noreply.github.com>
2026-08-14 17:11:47 +0200

    refactor: expose accessibility domain engine

--- src/domain/accessibility/index.ts
+++ src/domain/accessibility/index.ts
@@ -1,3 +1,7 @@
 export * from './contrast';
 export * from './key-contrast-pairs';
 export * from './generated-accessibility-summary';
+export * from './accessibility-rule-sources';
+export * from './accessibility-automated-rules';
+export * from './accessibility-score';
+export * from './accessibility-center';
